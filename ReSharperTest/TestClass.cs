7eb0445025c930d65613ea95ffcd80c4636b5df3
Leonid Lytvynenko <l.lytvynenko@gmail.com>
2012-01-31 14:25:03 +0200

    Initial commit

---
+++ ReSharperTest/TestClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReSharperTest
+{
+    internal interface ITestClass
+    {
+        void TestThis();
+    }
+
+    class TestClass : ITestClass
+    {
+        public void TestThis()
+        {
+            
+        }
+    }
+}
